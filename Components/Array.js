9916f4e61f2856e338f74550800e9819487ba065
Muhammad Nawfal Mehboob <nawfalmehboob@gmail.com>
2022-11-18 13:17:04 +0500

    MainForm Componnet created and added

---
+++ Components/Array.js
@@ -0,0 +1,17 @@
+import {
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableRow,
+  Typography,
+} from "@mui/material";
+import Head from "next/head";
+import classes from "../styles/Array.module.css";
+
+export default function Array() {
+  return (
+    <Head>
+      <Typography variant="h1">Array</Typography>
+    </Head>
+  );
+}
