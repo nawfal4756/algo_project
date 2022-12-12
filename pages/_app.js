164b9862523211f8103e6dbb4ca541ad6ca1c770
Muhammad Nawfal Mehboob <nawfalmehboob@gmail.com>
2022-12-12 10:37:16 +0500

    Page Title Added

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import { Paper } from "@mui/material";
+import Head from "next/head";
 import { Provider } from "react-redux";
 import AppBarPanel from "../Components/AppBarPanel";
 import SnackBarComponent from "../Components/SnackBarComponent";
@@ -9,6 +10,9 @@ import classes from "../styles/Home.module.css";
 function MyApp({ Component, pageProps }) {
   return (
     <Provider store={store}>
+      <Head>
+        <title>Algorithm Solver</title>
+      </Head>
       <Paper elevation={4} className={classes.container}>
         <AppBarPanel />
         <Component {...pageProps} />;
